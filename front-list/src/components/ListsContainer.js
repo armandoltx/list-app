0170af07cb5cace57cdae3111ca7c4ded9f8f686
armandoltx <armando@armandoltx.com>
2018-09-26 19:19:16 +1000

    Installed axios and created the get response

--- front-list/src/components/ListsContainer.js
+++ front-list/src/components/ListsContainer.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
+
 class ListsContainer extends Component {
+  state = {
+    lists: []
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/lists.json')
+    .then(response => {
+      console.log(response)
+      // this.serState({
+      //   lists: response.data
+      // })
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div className="Lists-container">
